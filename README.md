d469e28d373ad3a04cec43dd993a02274f41d36f
damiankam <demon.kaminsky@gmail.com>
2016-06-01 20:54:07 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Spring tutorial https://spring.io/guides/gs/producing-web-service/
+
+1. Start Application.
+2. $ curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws \
+    | tidy -xml -i -q
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dima on 6/1/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
